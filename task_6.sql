9e8275cb80dfc697770935c3d3d5aa472fdd757f
Chris Zantira <chrizantira@gmail.com>
2024-10-20 21:40:02 +0000

    update task 6

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 USE alx_book_store;
 USE Customers;
 
-INSERT INTO customers (customer_id, customer_name email, address) VALUES ("2", "Blessing Malik", "bmalik@sandtechcom", "124 Happiness Ave.");
+INSERT INTO customers (customer_id, customer_name email, address) VALUES ("2", "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave.");
 
 INSERT INTO customers (customer_id, customer_name, email, address ) VALUES ("3", "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave.");
     
