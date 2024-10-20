bdbdeb83d26a3100aaeee04aaab765184dbd54b4
Chris Zantira <chrizantira@gmail.com>
2024-10-20 21:26:46 +0000

    add task 6

---
+++ task_6.sql
@@ -0,0 +1,25 @@
+USE alx_book_store;
+USE Customers;
+
+INSERT INTO customers
+    customer_id = "2",
+    customer_name = "Blessing Malik",
+    email = "bmalik@santech.com",
+    address = "124 Happiness Ave.";
+
+INSERT INTO customers
+    customer_id = "3",
+    customer_name = "Obed Ehoneah",
+    email = "eobed@santech.com",
+    address = "125 Happiness Ave.";
+
+
+INSERT INTO Customers
+    customer_id = "4",
+    customer_name = "Nehemial Kamolu",
+    email = "nkamolu@snadtech.com",
+    address = "126 Happiness Ave.";
+
+
+
+
