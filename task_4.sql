099f075e63404250d91e626343b81acdfc4a1661
Chris Zantira <chrizantira@gmail.com>
2024-10-20 19:22:08 +0000

    update task 4

--- task_4.sql
+++ task_4.sql
@@ -10,5 +10,5 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'books';
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
