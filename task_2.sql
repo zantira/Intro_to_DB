b8c41f78851e2be54a01a59f30cb9821eea217dd
Chris Zantira <chrizantira@gmail.com>
2024-10-20 18:47:57 +0000

    update alx bookt store

--- task_2.sql
+++ task_2.sql
@@ -1,35 +1,35 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE books (
+CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR (130),
-    FOREIGN KEY (author_id) REFERENCES authors (author_id),
+    FOREIGN KEY (author_id) REFERENCES Authors (author_id),
     price DOUBLE,
     publication_date DATE
 );
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT PRIMARY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE orders(
+CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
-CREATE TABLE order_details(
+CREATE TABLE Order_Details(
     order_details_id PRIMARY KEY,
-    FOREIGN KEY (order_id) REFERENCES orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
